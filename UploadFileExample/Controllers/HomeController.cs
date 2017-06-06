6be9dfe5b70bef22f1df43f06f3f996c0560e857
Ampla contabilidade <Ampla contabilidade>
2017-06-06 19:06:44 -0300

    first commit

---
+++ UploadFileExample/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace UploadFileExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _enviroment;
+
+        public HomeController(IHostingEnvironment enviroment)
+        {
+            _enviroment = enviroment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult UploadFiles()
+        {
+            return View();
+        }
+
+        [Route("Upload")]
+        public IActionResult Upload(IFormFile file)
+        {
+            return View();
+        }
+    }
+}
